73278eedc39f6cd16e9e4b359807bc0b9634f659
Erick Menezes <erickdemenezes04@gmail.com>
2024-05-24 11:03:16 -0300

    while no main

--- pjblGasosaBet/src/Main.java
+++ pjblGasosaBet/src/Main.java
@@ -1,27 +1,49 @@
 import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Escolha o tipo de aposta: ");
-        System.out.println("1. Aposta Esportiva");
-        System.out.println("2. Aposta em Roleta");
-        int escolha = scanner.nextInt();
-        scanner.nextLine();
+        while (true){
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Escolha o tipo de aposta: ");
+            System.out.println("1. Aposta Esportiva");
+            System.out.println("2. Aposta em Roleta");
+            System.out.println("3. Visualizar histórico de apostas");
+            System.out.println("4. Sair");
+            int escolha = scanner.nextInt();
+            String entrada = scanner.nextLine();
 
-        if (escolha == 1) {
-            ApostaEsportiva minhaAposta = new ApostaEsportiva();
-            minhaAposta.definirValorAposta(scanner);
-            minhaAposta.definirAposta(scanner);
+            if (escolha == 1) {
+                ApostaEsportiva minhaAposta = new ApostaEsportiva();
+                minhaAposta.definirValorAposta(scanner);
+                minhaAposta.definirAposta(scanner);
 
-        } else if (escolha == 2) {
-            ApostaRoleta minhaAposta = new ApostaRoleta();
-            minhaAposta.definirValorAposta(scanner);
-            minhaAposta.resultadoFinal();
-        } else {
-            System.out.println("Escolha inválida.");
+            } else if (escolha == 2) {
+                ApostaRoleta minhaAposta = new ApostaRoleta();
+                minhaAposta.definirValorAposta(scanner);
+                minhaAposta.resultadoFinal();
+            }
+            else if (escolha == 3) {
+                // Leitura de histórico no arquivo
+                String nomeArquivo = "historico_apostas.txt";
+                try (BufferedReader leitor = new BufferedReader(new FileReader(nomeArquivo))) {
+                    String linha;
+                    while ((linha = leitor.readLine()) != null) {
+                        System.out.println(linha);
+                    }
+                } catch (IOException e) {
+                    System.err.println("Erro ao ler o arquivo: " + e.getMessage());
+                }
+            }
+            else if (escolha == 4) {
+                System.out.println("Encerrando o programa...");
+                break; // Sai do loop
+            }
+            else {
+                System.out.println("Escolha inválida.");
+            }
         }
-
-        Aposta.exibirHistoricoApostas();
     }
 }
